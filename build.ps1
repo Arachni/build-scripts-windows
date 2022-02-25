74362f6f755758f071e2235fb9d8a7574caf327e
Tasos Laskos <tasos.laskos@gmail.com>
2022-02-25 09:50:10 +0200

    Updated setenv

--- build.ps1
+++ build.ps1
@@ -107,7 +107,7 @@ function SetEnvBatch() {
 
 set ENV_ROOT=%~dp0
 set ENV_RUBY_BIN=%ENV_ROOT%ruby\bin
-set ENV_CHROMEDRIVER_DIR=%ENV_ROOT%..\chromedriver
+set ENV_CHROMEDRIVER_DIR=%ENV_ROOT%\..\chromedriver
 set ENV_WEBUI_ROOT=%ENV_ROOT%arachni-ui-web
 set ENV_WEBUI_BIN=%ENV_WEBUI_ROOT%\bin
 
@@ -120,7 +120,7 @@ set ARACHNI_WEBUI_LOGDIR=%ENV_ROOT%\logs\webui
 :: PhantomJS cache needs to be per package to prevent conflicts.
 set USERPROFILE=%ENV_ROOT%home
 
-For /F "Delims=" %%I In ('echo "%PATH%" ^| find /C /I "%ENV_RUBY_BIN%"') Do set pathExists=%%I 2>Nul
+For /F "Delims=" %%I In ('echo "%PATH%" ^| find /C /I "%ENV_CHROMEDRIVER_DIR%"') Do set pathExists=%%I 2>Nul
 If %pathExists%==0 set PATH=%ENV_RUBY_BIN%;%ENV_CHROMEDRIVER_DIR%;%PATH%
 "@
 }
